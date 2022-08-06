cdb073855a53e1b8eab692787b3b988182ea70d1
CHARANRAJEEV234 <109627469+CHARANRAJEEV234@users.noreply.github.com>
2022-08-06 23:08:57 +0530

    Delete code.js

--- code.js
+++
@@ -1,8 +0,0 @@
-function player()
-{
-
-        document.getElementById('welcome').innerHTML='<audio src="https://node-03.zeno.fm/sepgm12xuxhvv?zs=HHwoce8ITSSUpjpy3w9c9g&amp;rj-ttl=5&amp;rj-tok=AAABgizv1XYA0MqBOQHBDTqTig" type="audio/mpeg" controls></audio>'
-    
-        document.getElementById("btn").remove();
-    }
-    
